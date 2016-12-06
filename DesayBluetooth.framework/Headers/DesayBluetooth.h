f01fbc0817c32caedc7a3ece80daa71ca34749ca
ZhuYa <454040106@qq.com>
2016-12-06 11:45:06 +0800

    no message

---
+++ DesayBluetooth.framework/Headers/DesayBluetooth.h
@@ -0,0 +1,17 @@
+//
+//  DesayBluetooth.h
+//  DesayBluetooth
+//
+//  Created by Desay on 16/6/29.
+//  Copyright © 2016年 ran. All rights reserved.
+//
+
+#ifndef _DESAY_BLUETOOTH_H_
+#define _DESAY_BLUETOOTH_H_
+#import <CoreBluetooth/CoreBluetooth.h>
+#import "DSBLECentralManager.h"
+#import "DSBLEPeripheral.h"
+#import "DSBLEBaseProtocol.h"
+#import "DSBLESyncRequestDataModel.h"
+#import "DSBLEUtils.h"
+#endif
